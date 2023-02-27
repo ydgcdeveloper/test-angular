262194dd64ce05caaebb53c17df117d3ac8908c3
Yan David <yan.gomez@qualud.es>
2023-02-27 00:20:31 -0500

    add components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { ROOT_REDUCERS } from './state/app.state';
 import { productsReducer } from './state/reducers/products.reducer';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ProductComponent } from './component/product/product.component';
 import { ProductUiComponent } from './component/product-ui/product-ui.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { EditProductComponent } from './component/edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductUiComponent
+    ProductUiComponent,
+    AddProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
     StoreModule.forRoot(ROOT_REDUCERS),
-    StoreDevtoolsModule.instrument({ name: 'TEST' })
+    StoreDevtoolsModule.instrument({ name: 'TEST' }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
