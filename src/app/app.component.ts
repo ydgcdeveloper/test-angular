db5de68334fb9318245b5d85549638092da92598
Yan David <yan.gomez@qualud.es>
2023-02-26 21:53:57 -0500

    set app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { ProductModel } from './core/models/product.interface';
+import { selectProductsLoading } from './state/selectors/products.selector';
+import { loadedProducts, loadProducts } from './state/actions/products.action';
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductService } from './service/product/product.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-angular';
+
+  loading$: Observable<boolean> = new Observable();
+
+  constructor(
+    private store: Store<any>,
+    private productService: ProductService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loading$ = this.store.select(selectProductsLoading);
+
+    this.store.dispatch(loadProducts())
+
+    this.productService.getProducts().then((response: ProductModel[]) => {
+      console.log('value', response);
+      this.store.dispatch(loadedProducts({ products: response }))
+    });
+
+    // this.productService.addProduct();
+    // this.productService.removeProduct('bJnw8B8qXGDudC5TCTjj').then((value) => {
+    //   console.log('value deleted', value);
+    // });
+  }
+
 }
