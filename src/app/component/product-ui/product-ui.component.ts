0c55a2d8308a005d75decb8702ea7921c135a207
Yan David <yan.gomez@qualud.es>
2023-02-27 00:20:03 -0500

    set remove product confirmation modal

--- src/app/component/product-ui/product-ui.component.ts
+++ src/app/component/product-ui/product-ui.component.ts
@@ -1,5 +1,10 @@
+import { selectProductsList, selectProductsLoading } from './../../state/selectors/products.selector';
+import { Store } from '@ngrx/store';
+import { AppState } from './../../state/app.state';
 import { ProductModel } from './../../core/models/product.interface';
 import { Component, Input } from '@angular/core';
+import { ProductService } from 'src/app/service/product/product.service';
+import { loadedProducts, loadProducts } from 'src/app/state/actions/products.action';
 
 @Component({
   selector: 'app-product-ui',
@@ -8,4 +13,28 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductUiComponent {
   @Input() product: ProductModel;
+  showModalDelete: boolean = false;
+  showModalEdit: boolean = false;
+
+  constructor(
+    private store: Store<AppState>,
+    private productService: ProductService,
+  ) {
+
+  }
+
+  removeProduct() {
+    this.showModalDelete = false;
+    if (this.product.id) {
+      this.productService.removeProduct(this.product.id).then(() => {
+        this.getProducts();
+      });
+    }
+  }
+
+  getProducts() {
+    this.productService.getProducts().then((response: ProductModel[]) => {
+      this.store.dispatch(loadedProducts({ products: response }))
+    });
+  }
 }
