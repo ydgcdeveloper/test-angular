f57ed2dbfff18c2f0b2e78b3b69a49e8cd13f127
Yan David <yan.gomez@qualud.es>
2023-02-26 21:51:12 -0500

    set tailwind params

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  plugins: [],
+}
